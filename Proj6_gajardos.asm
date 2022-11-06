95bec2160c1d350ab3b08c3ee0c00526a37b866d
Chileancoder <62527730+Chileancoder@users.noreply.github.com>
2022-11-06 10:29:21 -0700

    Update Proj6_gajardos.asm

--- Proj6_gajardos.asm
+++ Proj6_gajardos.asm
@@ -4,15 +4,16 @@ TITLE  Final Project    (Proj6_gajardo.asm)
 ; Last Modified: 3/8/21
 ; OSU email address: gajardos@oregonstate.edu
 ; Course number/section:   CS271 Section 400
-; Project Number: 6                Due Date: 3/14/21
+; Project Number: 6  
+; Due Date: 3/14/21
 ; Description: Test program using macros and procedures to get 10 numbers, display them, their sum and average. Does this by calling the 
 ;			   ReadVal procedure which uses the mGetString macro to get the string from the user and the number of bytes read. Then 
 ;			   converts from character (ASCII) to signed integer form and saves the signed integer into memory, then fillarray is called 
 ;			   which inputs that integer into memory. This is looped 10 times until all 10 inputs are recieved and saved as signed integers
-;			   Then the sum and average is calculated vis the GetAvg procedure which saves it into memory and finally the Writeval 
+;			   Then the sum and average is calculated with the GetAvg procedure which saves it into memory and finally the Writeval 
 ;			   procedure is used to transform those integers back to string format and calls the mDisplayString to display them. Then the
 ;			   sum and finally the average. Then displays parting message. Also displays program name and programmer along with 
-;		       instructions prior to prompting user for initial signed integers that fit within 32 bits.
+;		       	   instructions prior to prompting user for initial signed integers that fit within 32 bits.
 
 
 
@@ -42,7 +43,7 @@ mGetString			MACRO prompt:REQ, input:REQ, count:REQ, read:REQ
 	MOV		ECX	, read												; Address reference read variable.
 	mov		[ECX], EAX	
 
-	pop		EAX														; Restore used registers.
+	pop		EAX													; Restore used registers.
 	pop		ECX														
 	pop		EDX
 ENDM
@@ -60,7 +61,7 @@ mDisplayString		MACRO input:REQ
 	mov		EDX, input
 	call	WriteString
 
-	pop		EDX														; Restore used register.
+	pop		EDX													; Restore used register.
 ENDM
 
 
@@ -81,8 +82,8 @@ count			DWORD	32
 read			DWORD	?
 numbers			SDWORD	10 DUP(?)										; Signed numbers to be saved.
 sizeNums		SDWORD	LENGTHOF numbers
-currentNum		SDWORD	?												; String number being converted
-sign			SDWORD	?												; Sign of string number being converted.
+currentNum		SDWORD	?											; String number being converted
+sign			SDWORD	?											; Sign of string number being converted.
 sum				SDWORD	?
 average			SDWORD	?
 numPrompt		BYTE	"You entered the following numbers:",13,10,0
@@ -116,7 +117,7 @@ _GetValues:
 	push	OFFSET read
 	call	ReadVal
 ; FillArray
-	push	ECX															; Current number out of 10.
+	push	ECX														; Current number out of 10.
 	push	OFFSET numbers
 	push	currentNum													; CurrentNum value.
 	call	FillArray
@@ -206,8 +207,8 @@ introduction		ENDP
 ; Procedure to read number string input by the use of the mGetString macro. Once the string number is read it converts from string
 ; to signed integer using string primitives by subtracting 48 from the ASCCI representation and then saves it in memory. If number is
 ; negative (sign = 1) it negates the number prior to saving it in memory (currentNum). Has input validation for numbers that don't fit 
-; in 32 bits for both positive a negative cases, it also validates i it's a number vs another character or letter. When the input is 
-; Invalid it displays an error message and re prompts.
+; in 32 bits for both positive a negative cases, it also validates it's a number vs another character or letter. When the input is 
+; invalid it displays an error message and re prompts.
 ; Preconditions: Prompt1, prompt2 and error are strings, mGetString is a macro, count is an SDWORD, sign is an integer initialized to 0, 
 ; read is a DWORD and input is an uninitialized string.
 ; Postconditions: None.
@@ -227,14 +228,14 @@ ReadVal				PROC
 
 _Input:														
 ; Macro used to get input string from the user.
-	mGetString [EBP + 20], [EBP + 16], [EBP + 12], [EBP + 8]			; Prompt1, input and read references and count value.
+	mGetString [EBP + 20], [EBP + 16], [EBP + 12], [EBP + 8]									; Prompt1, input and read references and count value.
 _Skip:
 	mov		ESI, [EBP + 8]											
 	mov		ECX, [ESI]													; Size of array input.
 	cmp		ECX, 0														; If no input, send to error.
 	JE		_Error
-	CLD																	; Clear direction flag.
-	mov		ESI, [EBP + 16]												; Input string reference address.
+	CLD																; Clear direction flag.
+	mov		ESI, [EBP + 16]													; Input string reference address.
 
 	_Convert:
 	; Convert string to signed integer.
@@ -249,8 +250,8 @@ _Skip:
 		CMP		AL, 57
 		JA		_Error
 		SUB		AL, 48
-		MOVSX	EAX, AL													; Extend to 32 bit.
-		mov		EDI, [EBP + 32]											; Put current number reference in EDI
+		MOVSX	EAX, AL														; Extend to 32 bit.
+		mov		EDI, [EBP + 32]												; Put current number reference in EDI
 		mov		EBX, [EDI]
 		imul	EBX, 10
 		JO		_Error
@@ -259,18 +260,18 @@ _Skip:
 		mov		[EDI], EBX												; CurrentNum*10 + converted number.
 		LOOP	_Convert
 		; Change sign if necessary.
-		mov		EBX, [EBP + 28]											; Sign reference address.
+		mov		EBX, [EBP + 28]												; Sign reference address.
 		mov 	EDX, [EBX]
 		cmp		EDX, 0
 		JE		_End
-		mov		EDI, [EBP + 32]											; Currentnum 
-		NEG		SDWORD PTR[EDI]											; Change to negative if sign != 0.
+		mov		EDI, [EBP + 32]												; Currentnum 
+		NEG		SDWORD PTR[EDI]												; Change to negative if sign != 0.
 		JO		_Error													; If negation causes overflow go to error.
 		JMP		_End
 
 	_Sign:
 	; If initial character check if it's + or - save sign and go to next character, if not check if it's number.
-		mov		EBX, [EBP + 28]											; Sign reference address.
+		mov		EBX, [EBP + 28]												; Sign reference address.
 		CMP		AL, 43													; +.
 		JE		_Positive										
 		CMP		AL, 45													; -.
@@ -290,7 +291,7 @@ _Error:
 ; Error message displayed if user enters invalid number, non number characters or no input.
 	mov		EDX, [EBP + 24]
 	call	WriteString
-	mGetString [EBP + 36], [EBP + 16], [EBP + 12], [EBP + 8]			; Prompt2, input and read references and count value.
+	mGetString [EBP + 36], [EBP + 16], [EBP + 12], [EBP + 8]									; Prompt2, input and read references and count value.
 	mov		EDX, [EBP + 32]
 	mov		EBX, 0
 	mov		[EDX], EBX													; Reset currenNum and sign to 0 if error occured.
@@ -299,7 +300,7 @@ _Error:
 	JMP		_Skip
 
 _End:
-	pop		EDI															; Restore used registers.
+	pop		EDI														; Restore used registers.
 	pop		ESI
 	pop		EAX
 	pop		EBX
@@ -325,14 +326,14 @@ FillArray			PROC
 	push	EDI
 	push	EAX
 
-	mov		ESI, [EBP + 8]												; Current number value
-	mov		EDI, [EBP + 12]												; Numbers array raference address.
-	mov		EAX, [EBP + 16]												; Number out of 10 total loops.
+	mov		ESI, [EBP + 8]													; Current number value
+	mov		EDI, [EBP + 12]													; Numbers array raference address.
+	mov		EAX, [EBP + 16]												;	 Number out of 10 total loops.
 	imul	EAX, 4		
 	add		EDI, EAX
 	mov		[EDI], ESI													; CurrentNum into array position.
 
-	pop		EAX															; Restore used registers.
+	pop		EAX														; Restore used registers.
 	pop		EDI
 	pop		ESI
 	pop		EBP
@@ -357,9 +358,9 @@ GetAverage				PROC
 	push	ECX
 	push	EBX
 
-	mov		ESI, [EBP + 16]												; Numbers array address reference.
-	mov		EDI, [EBP + 8]												; Sum address reference.
-	mov		ECX, [EBP + 20]												; Size of array.
+	mov		ESI, [EBP + 16]													; Numbers array address reference.
+	mov		EDI, [EBP + 8]													; Sum address reference.
+	mov		ECX, [EBP + 20]													; Size of array.
 	mov		EBX, 0														; Start displacement at 0.
 	mov		EAX, 0
 _Loop:
@@ -370,8 +371,8 @@ _Loop:
 	LOOP	_Loop
 ; End of sum loop now get average and store both values.
 	mov		[EDI], EAX													; Move sum to sum address reference.
-	mov		EBX, [EBP + 12]												; Average address references.
-	mov		ECX, [EBP + 20]												; Size of array.
+	mov		EBX, [EBP + 12]													; Average address references.
+	mov		ECX, [EBP + 20]													; Size of array.
 	CDQ																	
 	IDIV	ECX
 	mov		[EBX], EAX													; Average into average address reference.
@@ -387,7 +388,7 @@ GetAverage				ENDP
 ; ***************************************************************
 ; WriteVal
 ; Procedure converts numeric SDWORD value into string of ascii digits and calls mDisplayString to display it. Does this by continously 
-; diving SDWORD input by 10, appending remainder to passed string address and continuing until result is 0. Then null terminates the 
+; dividing SDWORD input by 10, appending remainder to passed string address and continuing until result is 0. Then null terminates the 
 ; string and reverses it before using macro to display it.
 ; Preconditions: Passed number value is a signed integer. Two string addresses are passed,
 ; Postconditions: None.
@@ -395,7 +396,7 @@ GetAverage				ENDP
 ; Returns: Displays sum value.
 ; ***************************************************************
 WriteVal			PROC
-	LOCAL	result: SDWORD												; Local variable to hold temp result.
+	LOCAL	result: SDWORD														; Local variable to hold temp result.
 	
 	push	ESI															; Save used registers.
 	push	EBX
@@ -404,8 +405,8 @@ WriteVal			PROC
 	push	EDX
 	push	EDI
 
-	mov		EAX, [EBP + 8]												; Sum reference by value.
-	mov		EDI, [EBP + 12]												; currentString address reference.
+	mov		EAX, [EBP + 8]													; Sum reference by value.
+	mov		EDI, [EBP + 12]													; currentString address reference.
 	ADD		EAX, 0
 	JNS		_Positive
 	NEG		EAX
@@ -440,19 +441,19 @@ _Reverse:
 	mov		ECX, EDI
 	SUB		EDI, 1
 	mov		ESI, EDI													; Read from end of currentString.
-	mov		EDI, [EBP + 16]												; Into begining of newString.
-	SUB		ECX, [EBP + 12]												; Get size of string into counter.
+	mov		EDI, [EBP + 16]													; Into begining of newString.
+	SUB		ECX, [EBP + 12]												;	 Get size of string into counter.
 _RevLoop:
 ; Reverses currentString into newString.
-	STD																	; Set Df.
+	STD																; Set Df.
 	LODSB
-	CLD																	; Clear df.
+	CLD																; Clear df.
 	STOSB
 	LOOP	_RevLoop
 	mov		AL, 0														; Null terminate reversed string.
 	STOSB
 ; Use macro to display newString.
-	mDisplayString [EBP + 16]											; Current string reference by address.
+	mDisplayString [EBP + 16]													; Current string reference by address.
 
 ; Restore used registers
 	pop		EDI								
